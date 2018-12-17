81dd42407f9bf94a679915f7dd147df39f2e7a0a
mdhvkothari <mdhvkothari007@gmail.com>
2018-12-17 13:43:31 +0530

    commit

---
+++ first.py
@@ -0,0 +1,25 @@
+from tkinter import * 
+g = Tk()
+g.title('Calculator')
+#size of the gui window 500x500 is size and 0 +0 is the loaction
+g.geometry('500x500+0+0')
+
+mylabe = Label(text='first label',fg='blue',bg='yellow').place(x=100,y=200)
+mylabe2= Label(text='second label' ,fg='red').place(x=100,y=300)
+
+mylabe3 = Label(text='third with grid').grid(row=0,column=0)
+
+mylabe4= Label(text='fourth gird').grid(row=1,column=0,stick='e')
+
+
+
+
+
+
+
+
+
+
+
+
+g.mainloop()
---
+++ title_name.py
@@ -0,0 +1,8 @@
+from tkinter import * 
+g = Tk()
+g.title('Calculator')
+#for sizing
+g.geometry('500x500+0+0')
+
+
+g.mainloop()
