180608abd4993931892d8c86e88a65523fdf2805
mdhvkothari <mdhvkothari007@gmail.com>
2018-12-24 15:07:01 +0530

    commit

--- button.py
+++ button.py
@@ -1,7 +1,15 @@
 from tkinter import * 
 g = Tk()
+
+def func1():
+	txt1 = txt.get()
+	Label(text=txt1,font=10).pack()
+
+
 g.title('Calculator')
 g.geometry('500x500+0+0')
+#for input to string
+txt = StringVar()
 
 mylabe = Label(text='first label',fg='blue',bg='yellow').place(x=100,y=200)
 mylabe2= Label(text='second label' ,fg='red').place(x=100,y=300)
@@ -10,8 +18,9 @@ mylabe3 = Label(text='third with grid').pack()
 
 mylabe4= Label(text='fourth gird').place(x=50,y=50)
 
-btn1 = Button(text='Submit',fg='green',bg='pink').place(x=70,y=100);
+btn1 = Button(text='Submit',fg='green',bg='pink',command=func1).place(x=70,y=100);
 
+myText = Entry(textvariable=txt).pack()
 
 
 
